2b6375b549b592dbfea8539db826a6bf2cfe244a
김정연 <jy.keem2468@gmail.com>
2023-07-04 19:42:54 +0900

    [FEAT]공통 컴포넌트(원룸, 투룸, 오피스텔) 구현

--- frontend/src/app/oneroom/page.tsx
+++ frontend/src/app/oneroom/page.tsx
@@ -1,68 +1,37 @@
+// 원룸 목업 데이터
 'use client'
 
-import { useEffect, useState, useRef } from 'react'
-import axios from 'axios'
+// import dynamic from 'next/dynamic'
 
-interface Property {
-  name: string
-  price: number
-  id: number
-}
+// const Property = dynamic(() => import('@/components/Property/Property'), {
+//   ssr: false,
+// })
 
-const Page = () => {
-  const [properties, setProperties] = useState<Property[]>([])
-  const [isLoading, setIsLoading] = useState(false)
-  const [page, setPage] = useState(1)
-  const containerRef = useRef<HTMLDivElement>(null)
+// const Page = () => {
+//   return (
+//     <>
+//       <Property />
+//     </>
+//   )
+// }
 
-  useEffect(() => {
-    fetchData()
-  }, [])
+// export default Page
 
-  useEffect(() => {
-    if (containerRef.current) {
-      const observer = new IntersectionObserver(handleObserver, {
-        threshold: 1,
-      })
-      observer.observe(containerRef.current)
-    }
-  }, [])
+import dynamic from 'next/dynamic'
 
-  const fetchData = async () => {
-    try {
-      setIsLoading(true)
-      const response = await axios.get('/api/mockup/properties?page=${page}')
-      setProperties((prevProperties) => [...prevProperties, ...response.data])
-      setPage((prevPage) => prevPage + 1)
-      setIsLoading(false)
-    } catch (error) {
-      console.error('Error fetching data:', error)
-      setIsLoading(false)
-    }
-  }
+const PropertyComponent = dynamic(
+  () => import('@/components/Property/PropertyComponent'),
+  {
+    ssr: false,
+  },
+)
 
-  const handleObserver = (entries: IntersectionObserverEntry[]) => {
-    const target = entries[0]
-    if (target.isIntersecting) {
-      fetchData()
-    }
-  }
-
+const OneroomPage = () => {
   return (
-    <div>
-      <h1>부동산 매물 목록</h1>
-      {properties.map((property, index) => (
-        <div key={index}>
-          <h3>{property.name}</h3>
-          <p>{property.price.toLocaleString()}원</p>
-          <p>{property.id}</p>
-        </div>
-      ))}
-      <div ref={containerRef} style={{ height: '100vh' }}>
-        {isLoading && <p>Loading...</p>}
-      </div>
-    </div>
+    <>
+      <PropertyComponent propertyType="oneroom" />
+    </>
   )
 }
 
-export default Page
+export default OneroomPage
