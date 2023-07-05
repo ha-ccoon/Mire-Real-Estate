2f715b5cca48843a9a46a93e063e4b8ef2c0de24
김정연 <jy.keem2468@gmail.com>
2023-07-05 16:09:42 +0900

    [PROG]오피스텔 상세 페이지 들어가야할 정보 주석으로 추가

--- frontend/src/app/officetel/[id]/page.tsx
+++ frontend/src/app/officetel/[id]/page.tsx
@@ -1,3 +1,11 @@
+import axios from 'axios'
+
+interface Property {
+  name: string
+  price: number
+  id: number
+}
+
 const OfficetelDetailPage = () => {
   return (
     <>
@@ -7,3 +15,19 @@ const OfficetelDetailPage = () => {
 }
 
 export default OfficetelDetailPage
+
+// 오피스텔
+// 위치
+// 월전세 + 보증금/월세
+// 관리비
+
+// 지어진 년도
+// 입주가능일
+// 전용 면적
+// 방향
+// 층수
+// 세대수
+// 방 개수??? 오피스텔에 방개수가 있나
+// 주차 여부
+
+// 위치(카카오 지도)
