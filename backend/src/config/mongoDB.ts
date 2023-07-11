cdebed434239d77cd837dc510d6b0b356b132179
minha sohn <codeminhasohn@gmail.com>
2023-07-11 20:33:49 +0900

    [DEL] 쓰지 않는 모듈과 코드 삭제

--- backend/src/config/mongoDB.ts
+++ backend/src/config/mongoDB.ts
@@ -1,7 +1,5 @@
 import dotenv from 'dotenv';
-import { Request, Response } from 'express';
 import mongoose from 'mongoose';
-import { resolve } from 'path';
 import logger from './winston';
 
 dotenv.config();
@@ -9,7 +7,6 @@ dotenv.config();
 const connectToMongoDB = async () => {
   try {
     const url = String(process.env.MONGO_URI);
-    const name = process.env.MONGODB_NAME;
 
     const connection = await mongoose.connect(url);
     logger.info('MongoDB is connected');
