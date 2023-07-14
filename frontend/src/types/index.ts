6bb52f77158100302891acbd22e8c1c032eaa01f
김정연 <jy.keem2468@gmail.com>
2023-07-14 21:00:36 +0900

    [ADD]전체 데이터 타입 추가

---
+++ frontend/src/types/index.ts
@@ -0,0 +1,10 @@
+interface MRE {
+  property_id: number // 매물 아이디
+  urgent_sale: number // 급매 여부
+  property_name: string // 매물 이름
+  property_type: string // 매물 타입
+  sale_method: string // 판매 유형
+  sale_price: string // 판매 가격
+  deposit: string // 보증금
+  premium: string //권리금
+}
