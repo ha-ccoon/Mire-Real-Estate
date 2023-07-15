5803770895705aa1afc640952718e587a094ab58
김정연 <jy.keem2468@gmail.com>
2023-07-15 23:33:31 +0900

    [PROG]오피스텔 매물 목록 데이터 목록 변경, return 수정

---
+++ frontend/src/app/officetel/details/[id]/page.tsx
@@ -0,0 +1,12 @@
+import { MRE } from '../../../../types'
+
+const OfficetelDetailPage = () => {
+  return (
+    <>
+      <p>오피스텔상세페이지입니당</p>
+      <p>위치</p>
+    </>
+  )
+}
+
+export default OfficetelDetailPage
--- frontend/src/components/PropertyComponent/PropertyComponent.tsx
+++ frontend/src/components/PropertyComponent/PropertyComponent.tsx
@@ -1,12 +1,20 @@
 import { useEffect, useState, useRef } from 'react'
 import axios from 'axios'
 import Link from 'next/link'
+import { MRE } from '../../types'
 
 // 매물 목록(오피스텔, 원룸, 투룸) 공통 컴포넌트
 interface Property {
-  name: string
-  price: number
-  id: number
+  property_name: string
+  sale_price: string
+  property_id: number
+  urgent_sale: number
+  property_type: string
+  deposit: string
+  exclusive_area: number
+  floor: number
+  total_floors: number
+  postal_code: number
 }
 
 interface PropertyComponentProps {
@@ -40,7 +48,21 @@ const PropertyComponent: React.FC<PropertyComponentProps> = ({
       const response = await axios.get(
         `/api/mockup/${propertyType}?page=${page}`,
       )
-      setProperties((prevProperties) => [...prevProperties, ...response.data])
+
+      const data: MRE[] = response.data
+      const processData: Property[] = data.map((mre: MRE) => ({
+        property_name: mre.property_name,
+        sale_price: mre.sale_price,
+        property_id: mre.property_id,
+        urgent_sale: mre.urgent_sale,
+        property_type: mre.property_type,
+        deposit: mre.deposit,
+        exclusive_area: mre.exclusive_area,
+        floor: mre.floor,
+        total_floors: mre.total_floors,
+        postal_code: mre.postal_code,
+      }))
+      setProperties((prevProperties) => [...prevProperties, ...processData])
       setPage((prevPage) => prevPage + 1)
       setIsLoading(false)
     } catch (error) {
@@ -60,14 +82,33 @@ const PropertyComponent: React.FC<PropertyComponentProps> = ({
     <div>
       <h1>부동산 매물 목록</h1>
       {properties.map((property, index) => (
-        <div key={index}>
+        <article key={index}>
           <h3>
-            <Link href={`/${propertyType}/${property.id}`}>
-              <p>{property.name}</p>
-              <p>{property.price.toLocaleString()}원</p>
+            <Link href={`/${propertyType}/details/${property.property_id}`}>
+              <p>
+                {property.urgent_sale === 1 ? (
+                  <span role="img" aria-label="Urgent">
+                    🚨
+                  </span>
+                ) : (
+                  ''
+                )}
+              </p>
+              <span>
+                {property.property_type}
+                {property.property_type === '월세' && (
+                  <span>{property.deposit}/</span>
+                )}
+                {property.sale_price.toLocaleString()}원
+              </span>
+              <p>{property.exclusive_area}평</p>
+              <p>
+                {property.floor}층 / {property.total_floors}층
+              </p>
+              <p>{property.postal_code}</p>
             </Link>
           </h3>
-        </div>
+        </article>
       ))}
       <div ref={containerRef} style={{ height: '100vh' }}>
         {isLoading && <p>Loading...</p>}
