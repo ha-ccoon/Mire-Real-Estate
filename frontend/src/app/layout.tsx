63c4a19dfb40e8b1fd3f908261fa67d8ff867ad8
woosung <jangwooseng97@gmail.com>
2023-07-18 23:39:58 +0900

    [FEAT] redux 초기 세팅 redux 설치 및 redux 모듈 추가

--- frontend/src/app/layout.tsx
+++ frontend/src/app/layout.tsx
@@ -3,6 +3,7 @@ import { Inter } from 'next/font/google'
 
 import Header from '../components/header/Header'
 import Footer from '../components/footer/Footer'
+import ReduxProvider from '@/components/redux-provider'
 
 const inter = Inter({ subsets: ['latin'] })
 
@@ -19,9 +20,11 @@ export default function RootLayout({
   return (
     <html lang="en">
       <body className={inter.className}>
-        <Header />
-        {children}
-        <Footer />
+        <ReduxProvider >
+          <Header />
+          {children}
+          <Footer />
+        </ReduxProvider>
       </body>
     </html>
   )
